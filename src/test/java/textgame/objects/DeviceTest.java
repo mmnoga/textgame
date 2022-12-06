1afb063c64d89a9beb1472cea849ef8f390596f0
Marcin Noga <marcin@careaboutit.pl>
2022-12-06 19:38:31 +0100

    corrected toString test in DeviceTest class

--- src/test/java/textgame/objects/DeviceTest.java
+++ src/test/java/textgame/objects/DeviceTest.java
@@ -7,72 +7,73 @@ import static org.junit.jupiter.api.Assertions.*;
 class DeviceTest {
     private Device unswitchableLamp = new Device("lamp", "lamp description",
             true, false,
-            false,false);
+            false, false);
     private Device lamp;
 
     @Test
-    void unswitchableDeviceShouldReturnCanNonBeSwitchedOnResponse(){
+    void unswitchableDeviceShouldReturnCanNonBeSwitchedOnResponse() {
         assertEquals("can not be turned on", unswitchableLamp.turnOn());
     }
 
     @Test
-    void unswitchableDeviceShouldReturnCanNonBeSwitchedOffResponse(){
+    void unswitchableDeviceShouldReturnCanNonBeSwitchedOffResponse() {
         assertEquals("can not be turned off", unswitchableLamp.turnOff());
     }
 
     private Device unvisibleLamp = new Device("lamp", "lamp description",
             false, false,
-            false,true);
+            false, true);
 
     @Test
-    void unvisibleDeviceShouldReturnThereSNoDeviceOnSwitchOn(){
+    void unvisibleDeviceShouldReturnThereSNoDeviceOnSwitchOn() {
         assertEquals("there's no device", unvisibleLamp.turnOn());
     }
 
     @Test
-    void unvisibleDeviceShouldReturnThereSNoDeviceOnSwitchOff(){
+    void unvisibleDeviceShouldReturnThereSNoDeviceOnSwitchOff() {
         assertEquals("there's no device", unvisibleLamp.turnOff());
     }
 
     private Device turnedOnLamp = new Device("lamp", "lamp description",
             true, false,
-            true,true);
+            true, true);
 
     @Test
-    void switchedOnDeviceShouldResponseItSAlreadyTurnedOn(){
+    void switchedOnDeviceShouldResponseItSAlreadyTurnedOn() {
         assertEquals("it's already turned on", turnedOnLamp.turnOn());
     }
 
     @Test
-    void switchedOffDeviceShouldResponseItSAlreadyTurnedOff(){
+    void switchedOffDeviceShouldResponseItSAlreadyTurnedOff() {
         turnedOnLamp.turnOff();
         assertEquals("it's already turned off", turnedOnLamp.turnOff());
     }
 
     private Device visibleLamp = new Device("lamp", "lamp description",
             true, false,
-            false,true);
+            false, true);
 
     @Test
-    void visibleSwitchOffCanBeSwitchedDeviceShoudReturnItSBeenTurnedOnResponse(){
+    void visibleSwitchOffCanBeSwitchedDeviceShouldReturnItSBeenTurnedOnResponse() {
         assertEquals("it's been turned on", visibleLamp.turnOn());
     }
 
     @Test
-    void visibleSwitchOnCanBeSwitchedDeviceShoudReturnItSBeenTurnedOffResponse(){
+    void visibleSwitchOnCanBeSwitchedDeviceShouldReturnItSBeenTurnedOffResponse() {
         lamp.turnOn();
         assertEquals("it's been turned off", lamp.turnOff());
     }
 
-    private final String lampDesc = "lamp (lamp description)";
-
     @Test
-    void toStringShouldReturnDeviceDescription(){
+    void toStringShouldReturnDeviceDescription() {
         lamp = new Device("lamp", "lamp description",
-                true,false,
-                false,true);
-        System.out.println(lamp.toString());
-//        assertEquals(lampDesc, lamp.toString());
+                true, false,
+                false, true);
+
+        String switchDesc = lamp.isSwitch() ? "it's switch on" : "it's switch off";
+        final String lampDesc = lamp.getName() + " (" + lamp.getDescription() + ")\n" + switchDesc;
+
+        assertEquals(lampDesc, lamp.toString());
     }
 
 }
