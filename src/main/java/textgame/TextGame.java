81cf08e07ccfe60c95b88ccc8258a07dd7487846
Marcin Noga <marcin@careaboutit.pl>
2022-12-06 09:54:37 +0100

    added basic project structure: game, helpers and object packages, and TextGame class with main method

---
+++ src/main/java/textgame/TextGame.java
@@ -0,0 +1,7 @@
+package textgame;
+
+public class TextGame {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/textgame/TextGameTest.java
@@ -0,0 +1,8 @@
+package textgame;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TextGameTest {
+}
